6fb07934b4bbb69440cfe0aada4939cc2817749a
Filip Hanik <fhanik@pivotal.io>
2014-10-24 10:41:07 -0600

    Fix whitespace

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/AuthorizationEndpoint.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/AuthorizationEndpoint.java
@@ -280,7 +280,7 @@ public class AuthorizationEndpoint extends AbstractEndpoint {
 	private View getAuthorizationCodeResponse(AuthorizationRequest authorizationRequest, Authentication authUser) {
 		try {
 			return new RedirectView(getSuccessfulRedirect(authorizationRequest,
-				generateCode(authorizationRequest, authUser)), false, true, false);
+					generateCode(authorizationRequest, authUser)), false, true, false);
 		}
 		catch (OAuth2Exception e) {
 			return new RedirectView(getUnsuccessfulRedirect(authorizationRequest, e, false), false, true, false);
